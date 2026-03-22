a071ad3d3a0325bdb0c712b7fece9e0ef0f6e3bb
y4m1x <nejcjamsek16@gmail.com>
2026-03-22 20:20:18 +0100

    Dodana funkcija za preverjanje sodosti stevila

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-hello
+#include <stdio.h>
+
+// Makro piši z velikimi črkami, da se opaz razlika od spremenljivk!
+#define STEVILO 17
+
+int je_sodo(int x) {
+    return x % 2 == 0;
+}
+
+int main() {
+    int rez = je_sodo(STEVILO);
+
+    #ifdef DEBUG
+        printf("DEBUG\n");
+    #else
+        printf("NO DEBUG\n");
+    #endif
+
+    if(rez) {
+        printf("%d je sodo stevilo.\n", STEVILO);
+    } else {
+        printf("%d ni sodo stevilo.\n", STEVILO);
+    }
+
+    return 0;
+}
